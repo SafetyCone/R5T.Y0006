fc8ebd1a22e45b54e598b1930a47ebf5c7808288
davidcoats <david@getsafetycone.com>
2024-01-18 01:24:43 -0800

    Interim changes.

---
+++ source/R5T.Y0006/Code/Links.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.L0073.Y0006
+{
+    public static partial class Links
+    {
+        /// <summary>
+        /// <value><see href="https://learn.microsoft.com/en-us/dotnet/core/tutorials/top-level-templates#global-using-directives"/></value>
+        /// </summary>
+        public static readonly object ForGlobalUsingDirectives;
+    }
+}
