e5ff3d4d3870f3333a46e43938c24bdc451a559c
davidcoats <david@getsafetycone.com>
2024-09-22 20:20:39 -0700

    Interim commit.

---
+++ source/R5T.Y0006/Code/Documentation/For-IDStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.T0156;
+
+
+namespace R5T.Y0006
+{
+    public static partial class Documentation
+    {
+        [DocumentationMarker]
+        public static partial class For_IDStrings
+        {
+            /// <summary>
+            /// <link><see href="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/xmldoc/#id-strings"/></link>
+            /// </summary>
+            public static readonly object MSDocs_APIDocumentationLink;
+
+            /// <summary>
+            /// <link><see href="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments#d42-id-string-format"/></link>
+            /// </summary>
+            public static readonly object MSDocs_DocumentationCommentsLink;
+
+            /// <summary>
+            /// <link><see href="https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments#d43-id-string-examples"/></link>
+            /// </summary>
+            public static readonly object MSDocs_Examples;
+        }
+    }
+}
--- source/R5T.Y0006/Code/Documentation/ForStrings.cs
+++ source/R5T.Y0006/Code/Documentation/ForStrings.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Xml.Linq;
 
 using R5T.T0156;
 
